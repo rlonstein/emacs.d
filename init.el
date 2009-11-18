17d233497601e9de239a16dabf313f27b0d21bb9
lonstein <lonstein@opal.local>
2009-11-18 08:28:32 -0500

    added breadcrumb; logging

--- init.el
+++ init.el
@@ -244,11 +244,26 @@ The value is an ASCII printing character (not upper case) or a symbol."
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
+;; William W. Wong's breadcrumb bookmarks
+(if (load "breadcrumb" t)
+    (progn
+      (global-set-key [(shift space)]         'bc-set)            ;; Shift-SPACE for set bookmark
+      (global-set-key [(meta j)]              'bc-previous)       ;; M-j for jump to previous
+      (global-set-key [(shift meta j)]        'bc-next)           ;; Shift-M-j for jump to next
+      (global-set-key [(meta up)]             'bc-local-previous) ;; M-up-arrow for local previous
+      (global-set-key [(meta down)]           'bc-local-next)     ;; M-down-arrow for local next
+      (global-set-key [(control c)(j)]        'bc-goto-current)   ;; C-c j for jump to current bookmark
+      (global-set-key [(control x)(meta j)]   'bc-list)           ;; C-x M-j for the bookmark menu list
+      (message "configured breadcrumb"))
+  (message "failed to load breadcrumb"))
+
 ;; smarter buffer naming
 (if (load "uniquify" t) 
     (progn 
       (setq uniquify-buffer-name-style 'post-forward)
-      (load "disbufnam" t)))
+      (load "disbufnam" t)
+      (message "configured uniquify"))
+  (message "failed to load uniquify"))
 
 (if +running-xemacs+ 
     (progn (require 'redo)
