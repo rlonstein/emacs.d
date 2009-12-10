292cc983697bff61a6ecbc42272c58f4e7dd5793
lonstein <lonstein@opal.local>
2009-12-10 18:14:47 -0500

    init.el reorg continues

--- init.el
+++ init.el
@@ -87,6 +87,8 @@ The value is an ASCII printing character (not upper case) or a symbol."
 (load "rel-load")
 (rel-load-dir-contents (concat (file-name-as-directory +init-file-path+) "rel-modules"))
 
+
+
 (let ((my-path-list (cond (+is-employer-host+ '("~/.custom/" 
                                            "~/.custom/elisp" 
                                            "~/.custom/elisp/misc" 
@@ -675,16 +677,6 @@ The value is an ASCII printing character (not upper case) or a symbol."
 
 
 
-
-;;
-;; Drew Adams's suggestion
-;;
-(defun create-scratch-buffer nil
-  "create a scratch buffer"
-  (interactive)
-  (switch-to-buffer (get-buffer-create "*scratch*"))
-  (lisp-interaction-mode))
-
 ;;
 ;; thanks to Edward O'Conner for these
 ;;
@@ -776,90 +768,6 @@ The value is an ASCII printing character (not upper case) or a symbol."
 (define-key global-map (kbd "M-(")  'myskeleton-pair-insert)
 (define-key global-map (kbd "M-[")  'myskeleton-pair-insert) ; brokeness on linux xterm?
 
-;;
-;; from c2.com wiki, GreatEmacsLispSnippets...
-;;
-(define-key global-map "\C-c\C-sf"
-    '(lambda () (interactive nil) (message "%s" (buffer-file-name))))
-
-(defun my-insert-timeofday ()
-  (interactive "*")
-  (insert (format-time-string "%a, %d %b %y %H:%M\n")))
-
-;;
-;; conveniences from Alex Schroeder
-;;
-(defun url-encode (str)
-  "URL-encode STR."
-  (interactive "sURL-encode: ")
-  (message "%s" (url-hexify-string str)))
-;(defalias 'url-escape 'url-encode)
-
-(defun url-decode (str)
-  "URL-decode STR."
-  (interactive "sURL-decode: ")
-  (message "%s" (decode-coding-string
-		 (url-unhex-string str)
-		 'utf-8)))
-
-
-;;
-;; Joel Holveck, use available spellcheck and set flyspell mode
-;;
-(defun have-executable (filename)
-  (some #'(lambda (dir)
-            (file-executable-p
-             (concat (file-name-as-directory dir) filename)))
-        exec-path))
-
-(cond
- ((have-executable "aspell")
-  (setq have-spell 'aspell)
-  (setq ispell-program-name "aspell"))
- ((have-executable "ispell")
-  (setq have-spell 'ispell)
-  (setq ispell-program-name "ispell"))
- (t
-  (setq have-spell nil)))
-
-; this might get annoying...
-(when have-spell  (add-hook 'text-mode-hook 'flyspell-mode))
-
-;;
-;; Joel Holveck, ssh via comint buffer
-;;
-(defun ssh (host)
-  "Open a secure network login connection to host named HOST (a string).
-    Communication with HOST is recorded in a buffer `*ssh-HOST*'.
-    Normally input is edited in Emacs and sent a line at a time."
-  (interactive "sOpen ssh connection to host: ")
-  (require 'telnet)
-  (require 'shell)
-  (let ((name (concat "ssh-" host )))
-    (pop-to-buffer (make-comint name remote-shell-program nil host))
-    (set-process-filter (get-process name) 'telnet-initial-filter)
-    (telnet-mode)
-    (setq telnet-count -16)))
-
-;;
-;; thanks to Dave Pearson
-;;
-;; added random seed
-;;
-(defun totd ()
-  (interactive)
-  (random t) ;
-  (with-output-to-temp-buffer "*Tip of the day*"
-    (let* ((commands (loop for s being the symbols
-                           when (commandp s) collect s))
-           (command (nth (random (length commands)) commands)))
-      (princ
-       (concat "Your tip for the day is:\n========================\n\n"
-               (describe-function command)
-               "\n\nInvoke with:\n\n"
-               (with-temp-buffer
-                 (where-is command t)
-                 (buffer-string)))))))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
