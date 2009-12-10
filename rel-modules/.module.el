b0c9d1b12375e328d2a19265e325281583182016
lonstein <lonstein@opal.local>
2009-12-10 18:20:53 -0500

    move Michele Bini's calc out of init.el

--- rel-modules/.module.el
+++ rel-modules/.module.el
@@ -1,3 +1,4 @@
 (setq modules '(("rel-sig.el" . t)
                 ("rel-conveniences.el" . t)
-                ("rel-borrowed-snippets.el" . t)))
+                ("rel-borrowed-snippets.el" . t)
+                ("mb-calc.el" .t)))
