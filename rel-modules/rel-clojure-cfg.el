59ceacdfb891421bf651db5e9a1f171722d53ec8
Ross Lonstein <rlonstein@pobox.com>
2011-05-31 20:58:46 -0400

    fixup clojure config

--- rel-modules/rel-clojure-cfg.el
+++ rel-modules/rel-clojure-cfg.el
@@ -5,10 +5,16 @@
 (autoload 'clojure-mode "clojure-mode" "Major mode for Clojure" t)
 (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
 
+(require 'paredit)
+
 (add-hook 'clojure-mode-hook (lambda () 
                                (paredit-mode t)
                                (slime-mode t)
-                               (turn-on-eldoc-mode)))
+                               (unload-feature 'slime-autodoc t)
+                               (turn-on-eldoc-mode)
+			       (define-key clojure-mode-map "{" 'paredit-open-brace)
+			       (define-key clojure-mode-map "}" 'paredit-close-brace)))
+
 
 (message "... set up clojure...")
 
